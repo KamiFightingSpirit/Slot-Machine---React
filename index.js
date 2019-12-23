d4eac3af90ed4d7649b186fb60ffbc9088416e37
Andrew DeHuff <55668557+KamiFightingSpirit@users.noreply.github.com>
2019-12-23 13:39:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function numPicker(){
-    return Math.floor(Math.random() * 3)+1;
-}
 
 
 class App extends React.Component {
     render(){
-        console.log(numPicker());
+       
         return(
             <SlotResults 
                 pineapple="🍍"
